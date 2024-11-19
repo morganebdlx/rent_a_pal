1361bf528db115cf22869aa387f35fe40d7ecc2d
Thibault Charron <thibs.cha@gmail.com>
2024-11-19 15:54:55 +0100

    create controller actions

--- app/controllers/pals_controller.rb
+++ app/controllers/pals_controller.rb
@@ -5,22 +5,41 @@ class PalsController < ApplicationController
   end
 
   def new
+    @pal = Pal.new
 
   end
 
   def create
-    
+    @pal = Pal.new(pal_params)
+    @pal.save
+    redirect_to pal_path(@path)
+
   end
 
   def edit
+    @pal = Pal.find(params[:id])
 
   end
 
   def update
+    @pal = Pal.find(params[:id])
+    @pal.update(pal_params)
+    redirect_to pal_path(@pal)
 
   end
 
   def destroy
+    @pal = Pal.find(params[:id])
+    @pal.destroy
+    redirect_to pals_path, status: :see_other
 
   end
+
+  private
+
+  def pal_params
+    params.require(:pal).permit(:username, :age, :city, :hobby)
+  end
+
+
 end
