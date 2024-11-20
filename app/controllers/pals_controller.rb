c67b65ea9a45bc4c62353ddea2f1f6eaa0d0bba6
Jean-Brieuc OLIER <jeanbrieuc.olier@gmail.com>
2024-11-20 11:30:21 +0100

    CSS et before action

--- app/controllers/pals_controller.rb
+++ app/controllers/pals_controller.rb
@@ -1,5 +1,13 @@
 class PalsController < ApplicationController
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :age, :city, :hobby])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :age, :city, :hobby])
+  end
+
   def index
     @pals = Pal.all
   end
