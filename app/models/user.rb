9292d24f97f3ff85bcebb34df1d9ec575747c55b
Thibault Charron <thibs.cha@gmail.com>
2024-11-21 16:45:59 +0100

    adding bokkings to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :bookings
-  has_many :pals
+  has_one :pal
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
